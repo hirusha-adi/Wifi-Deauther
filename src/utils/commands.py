6417c0712d8a27c30eb2e0bd0c8d9e42582b80ea
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-04-12 00:33:03 +0530

    print colored

--- src/utils/commands.py
+++ src/utils/commands.py
@@ -3,16 +3,17 @@ import subprocess
 import typing as t
 
 from src.adapters.wireless import WirelessAdapter
+from src.utils.console import *
 
 
 def __iwconfig():
     try:
         output = subprocess.check_output(["iwconfig"]).decode()
     except FileNotFoundError:
-        print(f"Unable to check for wireless adapters using the 'iwconfig' command. Output is 'iwconfig: command not found'")
+        red(f"Unable to check for wireless adapters using the 'iwconfig' command. 'iwconfig' command is not found")
         sys.exit()
 
-    adapters = [adapter.strip() for adapter in output.split('\n\n')]
+    adapters = [adapter.strip() for adapter in output.split("\n\n")]
     return adapters
 
 
