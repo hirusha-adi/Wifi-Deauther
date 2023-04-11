f0fb22b71a62a1ba9868e584c66e34de50322d33
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-04-11 23:18:16 +0530

    Update commands.py

--- src/utils/commands.py
+++ src/utils/commands.py
@@ -1,8 +1,11 @@
 import sys
 import subprocess
+import typing as t
+
+from src.adapters.wireless import WirelessAdapter
 
 
-def iwconfig():
+def __iwconfig():
     try:
         output = subprocess.check_output(["iwconfig"]).decode()
     except FileNotFoundError:
@@ -11,3 +14,7 @@ def iwconfig():
 
     adapters = [adapter.strip() for adapter in output.split('\n\n')]
     return adapters
+
+
+def getAdapters() -> t.List[WirelessAdapter]:
+    return [WirelessAdapter(output=a) for a in __iwconfig()]
